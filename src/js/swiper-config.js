08de89d7683f31112a284e22f2989a16a58922fe
= <eslp91@hotmail.com>
2025-03-16 17:39:39 +0330

    first

---
+++ src/js/swiper-config.js
@@ -0,0 +1,12 @@
+// swiper.js
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+  });
+  
